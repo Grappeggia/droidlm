a221bd76beaadc50b213cbd74f1655dad71699bb
grappeggia <marcosgrappeggia@gmail.com>
2026-05-03 11:11:44 -0700

    Bump version to v0.1.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "ai.droidlm"
         minSdk = 29
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.1.15"
+        versionCode = 17
+        versionName = "0.1.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
