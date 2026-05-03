886df0e0234c8659655a6efd4ccae9d99ff5573e
grappeggia <marcosgrappeggia@gmail.com>
2026-05-02 18:19:39 -0700

    Bump version to v0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ai.droidlm"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
